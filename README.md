30a929c72571cb977acc78d373310c44e6aeff1d
Wuotes <mailbox@xrtuen.com>
2022-08-01 10:15:18 -0500

    Finishing setup for PyPI.

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # mconf
 Manage multiple TOML configurations from a single module.
 
+`pip install mconf`
+
 [![codefactor](https://www.codefactor.io/repository/github/wuotes/mconf/badge?style=plastic)](https://www.codefactor.io/repository/github/wuotes/mconf/) [![circleci](https://circleci.com/gh/wuotes/mconf.svg?style=shield)](https://app.circleci.com/pipelines/github/wuotes/mconf) [![codecov](https://codecov.io/gh/wuotes/mconf/branch/main/graph/badge.svg)](https://codecov.io/gh/wuotes/mconf) 
 
 ```
 from mconf import mtoml
 
-class pets(mconf):
+class pets(mtoml):
     def __init__(self):
-        mtoml.__init__('.\PATH_TO_CONFIGS\')  # set the relative path to the config directory, defaults to the current directory
+        mtoml.__init__('.\PATH_TO_CONFIGS\')  # set the relative path to the config directory, defaults to the current directory or a previously set directory
 
         if not self.is_loaded('dogs'):  # all instances share the same files
             self.load('dogs')  # load a config file named "dogs.toml"
@@ -16,7 +18,9 @@ class pets(mconf):
         if not self.is_loaded('cats'):
             self.load('cats')  # load a config file named "cats.toml"
 
-        # This module should not throw any exceptions, if a method fails it returns either False or None.
+        # this module should not throw any exceptions, if a method fails it returns either False or None.
+
+        self.conf = mtoml('.\PATH_TO_CONFIGS\')  # this is also valid; self.conf.load(), self.conf.is_loaded(), etc
 
     def __del__(self):
         self.save_all()  # save all loaded configs
--- setup.py
+++ setup.py
@@ -1,21 +1,38 @@
 from setuptools import setup
 
+def read_file(filename: str) -> str:
+    text_result = r''
+
+    with open(filename, r'r') as the_file:
+        text_result = the_file.read()
+
+    return text_result
+
 setup(
-    name='mconf',
-    version='1.0.0',    
-    description='Manage multiple TOML configurations from a single module.',
-    url='https://github.com/wuotes/mconf',
-    author='Jordan Schaffrin',
-    author_email='mailbox@xrtuen.com',
-    license='Mozilla Public License 2.0',
-    python_requires='>=3.9',
-    packages=['mconf'],
-    install_requires=['tomli>=2.0.1',
-                      'tomli_w>=1.0.0',                     
+    name=r'mconf',
+    version=r'1.0.0',    
+    description=r'Manage multiple TOML configurations from a single module.',
+    long_description=read_file(r'./README.md'),
+    long_description_content_type=r'text/markdown',
+    url=r'https://github.com/wuotes/mconf',
+    download_url=r'https://pypi.org/project/mconf/',
+    author=r'Jordan Schaffrin',
+    author_email=r'mailbox@xrtuen.com',
+    license=r'Mozilla Public License 2.0',
+    python_requires=r'>=3.9',
+    packages=[r'mconf'],
+    install_requires=[r'tomli>=2.0.1',
+                      r'tomli_w>=1.0.0',                     
                       ],
 
     classifiers=[
-        'License :: OSI Approved :: MPL-2.0 License',
-        'Programming Language :: Python :: 3.9',
+        r'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+        r'Programming Language :: Python :: 3.9',
+        r'Programming Language :: Python :: 3.10',
+        r'Programming Language :: Python :: 3.11',
+        r'Programming Language :: Python :: 3.12',
+        r'Operating System :: Microsoft :: Windows',
+        r'Operating System :: POSIX :: Linux',
+        r'Operating System :: MacOS',
     ],
 )
