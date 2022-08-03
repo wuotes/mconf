8a776cd130065fea48ca94e9fa4cc7bef39b34e2
Wuotes <mailbox@xrtuen.com>
2022-08-03 18:48:52 -0500

    Fixing a method scoping bug.

--- mtoml/__init__.py
+++ mtoml/__init__.py
@@ -5,3 +5,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this #
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.            #
 #######################################################################
+
+from .mtoml import (mtomlc, get_dir, set_dir, load, is_loaded, save, save_all, get, set)
